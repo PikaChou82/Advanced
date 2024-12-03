cf463ab7a0238fd15ae86c57c17f8e8b073ff370
PikaChou82 <audrey.saussaye@gmail.com>
2024-12-03 15:06:18 +0100

    Update Streamlit_3.py

--- Streamlit_3.py
+++ Streamlit_3.py
@@ -1,13 +1,12 @@
 import  streamlit  as  st 
 from streamlit_option_menu import option_menu
 import  streamlit_authenticator  as  stauth
-import os
 import  yaml 
 from  yaml.loader  import  SafeLoader
 
-yaml_file_path = os.path.join("https://raw.githubusercontent.com/PikaChou82/Advanced/refs/heads/main/","authentificate.yaml")  
-with open(yaml_file_path, 'r') as file:
-    config  =  yaml.load(file ,  Loader = SafeLoader )
+yaml_file_path = "https://raw.githubusercontent.com/PikaChou82/Advanced/refs/heads/main/authentificate.yaml")  
+with open(yaml_file_path) as file:
+   config  =  yaml.load(file ,  Loader = SafeLoader )
 
 # Pré-hachage de tous les mots de passe en texte brut une fois 
 # stauth.Hasher.hash_passwords(config['credentials'])
